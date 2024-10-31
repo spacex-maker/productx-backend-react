ed2e93a254fc90e78ca63744692519c2bbe9f80a
jerry <wzdhfn@gmail.com>
2024-11-01 01:16:09 +0400

    部门管理基础设施 部门员工管理基础设施

--- productx/src/views/base/adminDepartment/AdminDepartment.js
+++ productx/src/views/base/adminDepartment/AdminDepartment.js
@@ -1,21 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import { Input, Button, List } from 'antd';
 import api from 'src/axiosInstance';
+import {CListGroup, CListGroupItem} from "@coreui/react";
 
 const AdminDepartments = () => {
   const [departments, setDepartments] = useState([]);
   const [parentId, setParentId] = useState(1); // 初始父级部门 ID
-  const [parentParentId, setParentParentId] = useState(1); // 初始父级部门 ID
+  const [parentHistory, setParentHistory] = useState([1]); // 用于存储父级部门 ID 的历史
   const [searchTerm, setSearchTerm] = useState('');
+
   useEffect(() => {
     fetchDepartments(parentId);
-  },[]);
+  }, [parentId]);
 
   const fetchDepartments = async (id) => {
     try {
       const response = await api.get('/manage/admin-departments/list', {
         params: { parentId: id }
       });
+
+
       setDepartments(response);
     } catch (error) {
       console.error('Error fetching departments:', error);
@@ -23,13 +27,15 @@ const AdminDepartments = () => {
   };
 
   const handleDepartmentClick = (id) => {
-    fetchDepartments(id);
-    setParentParentId(parentId)
+    setParentHistory([...parentHistory, parentId]); // 保存当前 parentId 到历史记录
+    setParentId(id); // 更新当前 parentId
   };
 
   const handleBack = () => {
-    fetchDepartments(parentId);
-    setParentParentId(parentId)
+    // 从历史中获取上一个 parentId
+    const previousId = parentHistory[parentHistory.length - 1];
+    setParentId(previousId); // 更新当前 parentId
+    setParentHistory(parentHistory.slice(0, -1)); // 移除最后一个历史记录
   };
 
   const handleSearch = (e) => {
@@ -41,27 +47,35 @@ const AdminDepartments = () => {
   );
 
   return (
-    <div style={{ display: 'flex', height: '100vh', margin: 0 }}>
-      <div style={{ width: '300px', padding: '10px', borderRight: '1px solid #f0f0f0' }}>
+    <div style={{ display: 'flex', height: '100vh'}}>
+      <div style={{ width: '200px', borderRight: '1px solid #f0f0f0' }}>
         <Input
+          size="small"
           placeholder="搜索部门"
           value={searchTerm}
           onChange={handleSearch}
-          style={{ marginBottom: '8px', height: '32px' }}
+          style={{ marginBottom: '8px'}}
+          allowClear // 添加这个属性
         />
-        <Button onClick={handleBack} style={{ marginBottom: '8px', height: '32px' }} disabled={!parentId}>
+        <Button
+          size="small"
+          onClick={handleBack} style={{ marginBottom: '8px'}} disabled={parentHistory.length <= 1}>
           返回上一级
         </Button>
-        <List
+        <CListGroup
           bordered
-          dataSource={filteredDepartments}
-          renderItem={item => (
-            <List.Item onClick={() => handleDepartmentClick(item.id)} style={{ cursor: 'pointer' }}>
-              {item.name}
-            </List.Item>
-          )}
           style={{ maxHeight: 'calc(100vh - 100px)', overflowY: 'auto' }} // 设置列表最大高度并使其可滚动
-        />
+        >
+          {filteredDepartments.map(item => (
+            <CListGroupItem
+              key={item.id}
+              onClick={() => handleDepartmentClick(item.id)}
+              style={{ cursor: 'pointer' }}
+            >
+              {item.name}
+            </CListGroupItem>
+          ))}
+        </CListGroup>
       </div>
       <div style={{ flex: 1, padding: '20px' }}>
         {/* 在这里显示所选部门的详细信息 */}
---
+++ productx/src/views/base/manager/Manager.js
@@ -0,0 +1,228 @@
+import React, { useState, useEffect } from 'react';
+import api from 'src/axiosInstance';
+import { Modal, Button, Form, Input, message, Spin, Select, Col, Row } from 'antd';
+import { UseSelectableRows } from 'src/components/common/UseSelectableRows';
+import { HandleBatchDelete } from 'src/components/common/HandleBatchDelete';
+import Pagination from "src/components/common/Pagination";
+import ManagerTable from "src/views/base/manager/ManagerTable"; // 请确保你有相应的管理员表格组件
+import UpdateManagerModal from "src/views/base/manager/UpdateManagerModal"; // 更新管理员模态框
+import ManagerCreateFormModal from "src/views/base/manager/ManagerCreateFormModal"; // 新建管理员模态框
+
+const updateManagerStatus = async (id, newStatus) => {
+  await api.post('/manage/manager/change-status', { id, status: newStatus ? 1 : 0 });
+}
+
+const createManager = async (managerData) => {
+  await api.post('/manage/manager/create', managerData);
+}
+
+const updateManager = async (updateData) => {
+  await api.put(`/manage/manager/update`, updateData);
+}
+
+const ManagerList = () => {
+  const [data, setData] = useState([]);
+  const [totalNum, setTotalNum] = useState(0);
+  const [currentPage, setCurrent] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const [searchParams, setSearchParams] = useState({
+    username: '',
+    email: '',
+    phone: '',
+    roleId: undefined,
+    status: undefined,
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [isCreateModalVisible, setIsCreateModalVisible] = useState(false);
+  const [createForm] = Form.useForm();
+  const [isUpdateModalVisible, setIsUpdateModalVisible] = useState(false);
+  const [updateForm] = Form.useForm();
+  const [selectedManager, setSelectedManager] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, [currentPage, pageSize, searchParams]);
+
+  const {
+    selectedRows,
+    selectAll,
+    handleSelectAll,
+    handleSelectRow,
+  } = UseSelectableRows();
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const filteredParams = Object.fromEntries(
+        Object.entries(searchParams).filter(([_, value]) => value !== '' && value !== null)
+      );
+      const response = await api.get('/manage/manager/list', {
+        params: { currentPage, size: pageSize, ...filteredParams },
+      });
+
+      if (response.data) {
+        setData(response.data); // 更新数据结构
+        setTotalNum(response.totalNum); // 获取总记录数
+      }
+    } catch (error) {
+      console.error('Failed to fetch data', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleSearchChange = (event) => {
+    const { name, value } = event.target;
+    setSearchParams((prevParams) => ({ ...prevParams, [name]: value }));
+  };
+
+  const handleCreateManager = async (values) => {
+    await createManager(values);
+    setIsCreateModalVisible(false);
+    createForm.resetFields();
+    await fetchData();
+  };
+
+  const handleUpdateManager = async (values) => {
+    await updateManager(values);
+    setIsUpdateModalVisible(false);
+    updateForm.resetFields();
+    await fetchData();
+  };
+
+  const handleEditClick = (manager) => {
+    updateForm.setFieldsValue({
+      id: manager.id,
+      username: manager.username,
+      email: manager.email,
+      phone: manager.phone,
+      roleId: manager.roleId,
+      status: manager.status,
+    });
+    setIsUpdateModalVisible(true);
+  };
+
+  const totalPages = Math.ceil(totalNum / pageSize);
+
+  return (
+    <div>
+      <div className="mb-3">
+        <div className="search-container">
+          <Row gutter={[16, 16]}>
+            <Col>
+              <Input
+                size="small"
+                value={searchParams.username}
+                onChange={handleSearchChange}
+                name="username"
+                placeholder="用户名"
+                allowClear
+              />
+            </Col>
+            <Col>
+              <Input
+                size="small"
+                value={searchParams.email}
+                onChange={handleSearchChange}
+                name="email"
+                placeholder="邮箱"
+                allowClear
+              />
+            </Col>
+            <Col>
+              <Input
+                size="small"
+                value={searchParams.phone}
+                onChange={handleSearchChange}
+                name="phone"
+                placeholder="手机号"
+                allowClear
+              />
+            </Col>
+            <Col>
+              <Select
+                size="small"
+                name="status"
+                onChange={(value) => handleSearchChange({ target: { name: 'status', value } })}
+                allowClear
+                placeholder="状态"
+              >
+                <Select.Option value={true}>启用</Select.Option>
+                <Select.Option value={false}>禁用</Select.Option>
+              </Select>
+            </Col>
+            <Col>
+              <Button
+                size="small"
+                type="primary"
+                onClick={fetchData}
+                disabled={isLoading}
+              >
+                {isLoading ? <Spin /> : '查询'}
+              </Button>
+            </Col>
+            <Col>
+              <Button
+                size="small"
+                type="primary"
+                onClick={() => setIsCreateModalVisible(true)}>
+                新增管理员
+              </Button>
+            </Col>
+            <Col>
+              <Button
+                size="small"
+                type="primary"
+                onClick={() => HandleBatchDelete({
+                  url: '/manage/manager/delete-batch',
+                  selectedRows,
+                  fetchData,
+                })}
+                disabled={selectedRows.length === 0}
+              >
+                批量删除
+              </Button>
+            </Col>
+          </Row>
+        </div>
+      </div>
+
+      <div className="table-responsive">
+        <Spin spinning={isLoading}>
+          <ManagerTable
+            data={data}
+            selectAll={selectAll}
+            selectedRows={selectedRows}
+            handleSelectAll={handleSelectAll}
+            handleSelectRow={handleSelectRow}
+            handleEditClick={handleEditClick}
+          />
+        </Spin>
+      </div>
+      <Pagination
+        totalPages={totalPages}
+        current={currentPage}
+        onPageChange={setCurrent}
+        pageSize={pageSize}
+        onPageSizeChange={setPageSize}
+      />
+      <ManagerCreateFormModal
+        isVisible={isCreateModalVisible}
+        onCancel={() => setIsCreateModalVisible(false)}
+        onFinish={handleCreateManager}
+        form={createForm}
+      />
+      <UpdateManagerModal
+        isVisible={isUpdateModalVisible}
+        onCancel={() => setIsUpdateModalVisible(false)}
+        onOk={() => updateForm.submit()}
+        form={updateForm}
+        handleUpdateManager={handleUpdateManager}
+        selectedManager={selectedManager} // 传递所选管理员信息
+      />
+    </div>
+  );
+};
+
+export default ManagerList;
