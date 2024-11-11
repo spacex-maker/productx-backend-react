f7e5357bb526cdd43a8ffcc9dcf00ff8c8a46cc4
spacex-maker <wzdhfn@gmail.com>
2024-11-11 11:06:38 +0400

    优化用户管理 优化国际化词语

--- productx/src/views/base/userList/UpdateUserModal.js
+++ productx/src/views/base/userList/UpdateUserModal.js
@@ -1,5 +1,7 @@
-import React, {useEffect} from 'react';
-import { Modal, Form, Input } from 'antd';
+import React, { useEffect } from 'react';
+import { Modal, Form, Input, Checkbox, Row, Col, Divider, Typography } from 'antd';
+
+const { Title } = Typography;
 
 const UpdateUserModal = ({
                            isVisible,
@@ -7,82 +9,161 @@ const UpdateUserModal = ({
                            onOk,
                            form,
                            handleUpdateUser,
-                           selectedUser // 新增的 props，用于传递选中的用户信息
+                           selectedUser
                          }) => {
-
   // 当模态框打开时，设置表单字段的值
   useEffect(() => {
     if (isVisible && selectedUser) {
       form.setFieldsValue({
         id: selectedUser.id,
+        description: selectedUser.description,
+        email: selectedUser.email,
+        status: selectedUser.status,
         nickname: selectedUser.nickname,
-        country: selectedUser.country,
-        state: selectedUser.state,
-        city: selectedUser.city,
+        phoneNumber: selectedUser.phoneNumber,
+        fullName: selectedUser.fullName,
         address: selectedUser.address,
+        creditScore: selectedUser.creditScore,
+        city: selectedUser.city,
+        state: selectedUser.state,
         postalCode: selectedUser.postalCode,
+        country: selectedUser.country,
+        isActive: selectedUser.isActive,
       });
     }
   }, [isVisible, selectedUser, form]);
 
   return (
     <Modal
-      title="修改用户"
+      title="编辑用户信息"
       open={isVisible}
       onCancel={onCancel}
       onOk={onOk}
+      width={500} // 设置模态框宽度为500px
     >
-      <Form form={form} onFinish={handleUpdateUser}>
+      <Form
+        form={form}
+        onFinish={handleUpdateUser}
+        layout="vertical"
+        style={{ gap: '8px' }} // 使用 gap 控制输入框之间的间距
+      >
         <Form.Item name="id" hidden>
           <Input />
         </Form.Item>
 
-        <Form.Item
-          label="用户昵称"
-          name="nickname"
-          rules={[{ required: true, message: '请输入用户昵称' }]}
-        >
-          <Input />
+        {/* 基本信息部分 */}
+        <Title level={5} style={{ marginBottom: '16px' }}>基本信息</Title>
+        <Divider style={{ margin: '8px 0' }} />
+
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              label="昵称"
+              name="nickname"
+              rules={[{ required: true, message: '请输入用户昵称' }]}
+              style={{ marginBottom: '12px' }} // 缩小下边距
+            >
+              <Input placeholder="请输入昵称" />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="全名" name="fullName" style={{ marginBottom: '12px' }}>
+              <Input placeholder="请输入全名" />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item label="电子邮件" name="email" style={{ marginBottom: '12px' }}>
+              <Input placeholder="请输入电子邮件" />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="电话号码" name="phoneNumber" style={{ marginBottom: '12px' }}>
+              <Input placeholder="请输入电话号码" />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item label="用户介绍" name="description" style={{ marginBottom: '12px' }}>
+              <Input placeholder="请输入用户介绍" />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="信誉分" name="creditScore" style={{ marginBottom: '12px' }}>
+              <Input type="number" placeholder="请输入信誉分" />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Form.Item label="状态" name="status" valuePropName="checked" style={{ marginBottom: '12px' }}>
+          <Checkbox>激活</Checkbox>
         </Form.Item>
 
-        <Form.Item
-          label="国家"
-          name="country"
-          rules={[{ required: true, message: '请输入国家' }]}
-        >
-          <Input />
-        </Form.Item>
+        {/* 地址信息部分 */}
+        <Title level={5} style={{ marginBottom: '16px' }}>地址信息</Title>
+        <Divider style={{ margin: '8px 0' }} />
+
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              label="国家"
+              name="country"
+              rules={[{ required: true, message: '请输入国家' }]}
+              style={{ marginBottom: '12px' }}
+            >
+              <Input placeholder="请输入国家" />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              label="州/省"
+              name="state"
+              rules={[{ required: true, message: '请输入州/省' }]}
+              style={{ marginBottom: '12px' }}
+            >
+              <Input placeholder="请输入州/省" />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              label="城市"
+              name="city"
+              rules={[{ required: true, message: '请输入城市' }]}
+              style={{ marginBottom: '12px' }}
+            >
+              <Input placeholder="请输入城市" />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              label="邮政编码"
+              name="postalCode"
+              rules={[{ required: true, message: '请输入邮政编码' }]}
+              style={{ marginBottom: '12px' }}
+            >
+              <Input placeholder="请输入邮政编码" />
+            </Form.Item>
+          </Col>
+        </Row>
 
         <Form.Item
-          label="州"
-          name="state"
-          rules={[{ required: true, message: '请输入州' }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="城市"
-          name="city"
-          rules={[{ required: true, message: '请输入城市' }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="默认地址"
+          label="详细地址"
           name="address"
-          rules={[{ required: true, message: '请输入默认地址' }]}
+          rules={[{ required: true, message: '请输入详细地址' }]}
+          style={{ marginBottom: '12px' }}
         >
-          <Input />
+          <Input placeholder="请输入详细地址" />
         </Form.Item>
 
-        <Form.Item
-          label="邮政编码"
-          name="postalCode"
-          rules={[{ required: true, message: '请输入邮政编码' }]}
-        >
-          <Input />
+        <Form.Item label="激活状态" name="isActive" style={{ marginBottom: '0px' }}>
+          <Input disabled placeholder="激活状态" />
         </Form.Item>
       </Form>
     </Modal>
