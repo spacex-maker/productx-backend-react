2225b43e6ae54340c5e0cb2aa4bbc8bbfffed01d
spacex-maker <wzdhfn@gmail.com>
2024-11-21 17:02:12 +0400

    新增AI助手

--- productx/src/views/pages/login/LoginPage.js
+++ productx/src/views/pages/login/LoginPage.js
@@ -440,7 +440,7 @@ const LoginPage = () => {
   };
 
   const handleGitHubLogin = () => {
-    window.location.href = 'https://github.com/login/oauth/authorize?client_id=Ov23liKtBY8tbrKGO1q2&redirect_uri=https://productx-manage.netlify.app';
+    window.location.href = 'https://github.com/login/oauth/authorize?client_id=Ov23liKtBY8tbrKGO1q2&redirect_uri=https://protx.cn/manage/manager/github-callback';
   };
 
   // 定义随机初始位置
