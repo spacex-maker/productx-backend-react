515f8a75e01b84b99f50cad70b5bb4a9529666d4
spacex-maker <wzdhfn@gmail.com>
2024-11-27 16:25:43 +0400

    丰富用户详情 邀请码生成-为代理系统准备 水印 国际化持续集成

---
+++ productx/src/redux/userSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    currentUser: null,
+  },
+  reducers: {
+    setCurrentUser: (state, action) => {
+      state.currentUser = action.payload;
+    },
+    clearCurrentUser: (state) => {
+      state.currentUser = null;
+    },
+  },
+});
+
+export const { setCurrentUser, clearCurrentUser } = userSlice.actions;
+export default userSlice.reducer; 
