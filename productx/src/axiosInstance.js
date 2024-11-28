99019e9141682bb713ad6679d919eeee0f5c989b
hayden <hayden@topluck999.com>
2024-11-28 19:07:59 +0800

    工程化调整

--- productx/src/axiosInstance.js
+++ productx/src/axiosInstance.js
@@ -7,7 +7,7 @@ export const API_CONFIG = {
   TEST: 'http://34.92.193.186:8090',
   TEST2: 'http://34.92.218.25:18090',
   PROD: 'https://protx.cn',
-  LOCAL: 'http://localhost:8090'
+  LOCAL: 'http://localhost:8090',
 };
 
 // 自动判断当前环境
@@ -25,6 +25,12 @@ const autoDetectEnvironment = () => {
 // 将 const 改为 let，使其可以被修改
 export let API_BASE_URL = API_CONFIG[autoDetectEnvironment()];
 
+const API_BASE_URL_STORAGE_KEY = "__API_BASE_URL";
+
+(function setBaseUrlByStorage(){
+  API_BASE_URL = localStorage.getItem(API_BASE_URL_STORAGE_KEY) ?? API_BASE_URL;
+})()
+
 // 设置 API 基地址
 export const setBaseURL = (environment) => {
   const url = API_CONFIG[environment];
@@ -33,14 +39,15 @@ export const setBaseURL = (environment) => {
     return;
   }
   API_BASE_URL = url;
-  axiosInstance.defaults.baseURL = API_BASE_URL;
+  axiosInstance.defaults.baseURL = url;
+  localStorage.setItem(API_BASE_URL_STORAGE_KEY, url);
 
   // 更新环境提示信息
   const envNames = {
     PROD: '生产(测试库1)，主机2c2g-谷歌云-香港',
     TEST2: '测试(测试库1)，主机16c60g-内网-北京',
     TEST: '测试(测试库1)，主机2c8g-谷歌云-香港',
-    LOCAL: '本地'
+    LOCAL: '本地',
   };
   message.success(`API 基地址已切换到${envNames[environment]}环境: ${API_BASE_URL}`);
 };
@@ -64,18 +71,14 @@ const whiteList = [
 // 创建 axios 实例
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
-  withCredentials: true // 允许携带 Cookie
+  withCredentials: true, // 允许携带 Cookie
 });
 // 请求拦截器
 axiosInstance.interceptors.request.use(
   (config) => {
-    config.baseURL = API_BASE_URL; // 设置基础 URL
-    config.withCredentials = true; // 确保跨域请求时携带 Cookie
 
     // 检查请求路径是否在白名单中
-    const isWhitelisted = whiteList.some(path =>
-      config.url.includes(path)
-    );
+    const isWhitelisted = whiteList.some((path) => config.url.includes(path));
 
     // 只有不在白名单中的请求才添加 token
     if (!isWhitelisted) {
@@ -87,12 +90,11 @@ axiosInstance.interceptors.request.use(
 
     return config;
   },
-  (error) => Promise.reject(error)
+  (error) => Promise.reject(error),
 );
 
 axiosInstance.interceptors.response.use(
   (response) => {
-
     const { success, data, message: msg } = response.data;
 
     if (success) {
@@ -103,7 +105,6 @@ axiosInstance.interceptors.response.use(
     }
   },
   (error) => {
-
     if (error.response) {
       // 判断状态码
       const { status, error: errorType, message } = error.response.data;
@@ -119,13 +120,14 @@ axiosInstance.interceptors.response.use(
       message.error('请登录', 4);
     }
     return Promise.reject(error);
-  }
+  },
 );
 
 // 添加设置自定义基地址的函数
 export const setCustomBaseURL = (customUrl) => {
   API_BASE_URL = customUrl;
   axiosInstance.defaults.baseURL = customUrl;
+  localStorage.setItem(API_BASE_URL_STORAGE_KEY, customUrl);
   message.success(`已切换到自定义环境: ${customUrl}`);
 };
 
