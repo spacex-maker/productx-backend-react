0c0247dd72bf6dafcc9e473d640b2737d67227ac
spacex-maker <wzdhfn@gmail.com>
2024-12-05 15:17:35 +0400

    debug

---
+++ productx/vite.config.js
@@ -0,0 +1,51 @@
+const { defineConfig } = require('vite')
+const react = require('@vitejs/plugin-react')
+const path = require('node:path')
+const autoprefixer = require('autoprefixer')
+
+module.exports = defineConfig(() => {
+  return {
+    base: './',
+    build: {
+      outDir: 'dist',
+    },
+    css: {
+      modules: {
+        localsConvention: 'camelCaseOnly',
+      },
+      postcss: {
+        plugins: [
+          autoprefixer({}),
+        ],
+      },
+    },
+    esbuild: {
+      loader: 'jsx',
+      include: /src\/.*\.jsx?$/,
+      exclude: [],
+    },
+    optimizeDeps: {
+      force: true,
+      esbuildOptions: {
+        loader: {
+          '.js': 'jsx',
+        },
+      },
+    },
+    plugins: [react()],
+    resolve: {
+      alias: [
+        {
+          find: 'src/',
+          replacement: path.resolve(__dirname, 'src') + '/',
+        },
+      ],
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.scss'],
+    },
+    server: {
+      host: '0.0.0.0',
+      port: 3000,
+      proxy: {},
+    },
+  }
+})
