8d6f928ee21cde1333f3efc2cbef96be35f760d9
ted <ted987116@gmail.com>
2025-01-09 16:21:35 +0400

    系统钱包页面逻辑优化 系统银行页面逻辑优化 维修商户页面逻辑优化 系统钱包页面逻辑优化 以及所有页面的国际化

--- productx/src/views/base/sysCryptoCurrencies/UpdateCryptoCurrencyModal.js
+++ productx/src/views/base/sysCryptoCurrencies/UpdateCryptoCurrencyModal.js
@@ -1,69 +1,109 @@
-import React, { useEffect } from 'react';
-import { Modal, Form, Input, InputNumber } from 'antd';
+import React from 'react';
+import { Modal, Form, Input, Select, InputNumber, Space } from 'antd';
+import { useTranslation } from 'react-i18next';
 
-const UpdateCurrencyModal = ({
-                               isVisible,
-                               onCancel,
-                               onOk,
-                               form,
-                               handleUpdateCurrency,
-                               selectedCurrency // 用于传递选中的货币信息
-                             }) => {
-  // 当模态框打开时，设置表单字段的值
-  useEffect(() => {
-    if (isVisible && selectedCurrency) {
-      form.setFieldsValue({
-        id: selectedCurrency.id,
-        name: selectedCurrency.name,
-        code: selectedCurrency.code,
-        exchangeRate: selectedCurrency.exchangeRate,
-      });
-    }
-  }, [isVisible, selectedCurrency, form]);
+const UpdateCryptoCurrencyModal = ({
+  isVisible,
+  onCancel,
+  onOk,
+  form,
+  handleUpdateCurrency,
+}) => {
+  const { t } = useTranslation();
 
   return (
     <Modal
-      title="修改货币"
+      title={t('updateCrypto')}
       open={isVisible}
       onCancel={onCancel}
       onOk={onOk}
-      okText="确认"
-      cancelText="取消"
+      width={600}
     >
-      <Form form={form} onFinish={handleUpdateCurrency}>
+      <Form 
+        form={form} 
+        onFinish={handleUpdateCurrency}
+        layout="vertical"
+        autoComplete="off"
+      >
         <Form.Item name="id" hidden>
           <Input />
         </Form.Item>
 
-        <Form.Item
-          label="货币名称"
-          name="name"
-          rules={[{ required: true, message: '请输入货币名称' }]}
-          style={{ marginBottom: '8px' }} // 调整上下间距
-        >
-          <Input />
-        </Form.Item>
+        <Space direction="vertical" style={{ width: '100%' }} size="small">
+          <Form.Item
+            label={t('cryptoName')}
+            name="name"
+            rules={[{ required: true, message: t('pleaseInputCryptoName') }]}
+          >
+            <Input placeholder={t('cryptoNamePlaceholder')} />
+          </Form.Item>
 
-        <Form.Item
-          label="货币代码"
-          name="code"
-          rules={[{ required: true, message: '请输入货币代码' }]}
-          style={{ marginBottom: '8px' }} // 调整上下间距
-        >
-          <Input />
-        </Form.Item>
+          <Form.Item
+            label={t('cryptoSymbol')}
+            name="symbol"
+            rules={[{ required: true, message: t('pleaseInputCryptoSymbol') }]}
+          >
+            <Input placeholder={t('cryptoSymbolPlaceholder')} />
+          </Form.Item>
 
-        <Form.Item
-          label="汇率"
-          name="exchangeRate"
-          rules={[{ required: true, message: '请输入汇率' }]}
-          style={{ marginBottom: '8px' }} // 调整上下间距
-        >
-          <InputNumber style={{ width: '100%' }} />
-        </Form.Item>
+          <Form.Item
+            label={t('cryptoChineseName')}
+            name="chineseName"
+            rules={[{ required: true, message: t('pleaseInputCryptoChineseName') }]}
+          >
+            <Input placeholder={t('cryptoChineseNamePlaceholder')} />
+          </Form.Item>
+
+          <Form.Item
+            label={t('blockchainType')}
+            name="blockchainType"
+            rules={[{ required: true, message: t('pleaseSelectBlockchainType') }]}
+          >
+            <Select placeholder={t('blockchainTypePlaceholder')}>
+              <Select.Option value="ETH">Ethereum</Select.Option>
+              <Select.Option value="BSC">Binance Smart Chain</Select.Option>
+              <Select.Option value="TRX">TRON</Select.Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item
+            label={t('transactionSpeed')}
+            name="transactionSpeed"
+            rules={[{ required: true, message: t('pleaseInputTransactionSpeed') }]}
+          >
+            <InputNumber 
+              style={{ width: '100%' }} 
+              min={1}
+              placeholder={t('transactionSpeedPlaceholder')}
+              addonAfter="TPS"
+            />
+          </Form.Item>
+
+          <Form.Item
+            label={t('website')}
+            name="website"
+            rules={[
+              { required: true, message: t('pleaseInputWebsite') },
+              { type: 'url', message: t('invalidUrl') }
+            ]}
+          >
+            <Input placeholder={t('websitePlaceholder')} />
+          </Form.Item>
+
+          <Form.Item
+            label={t('whitepaper')}
+            name="whitepaperUrl"
+            rules={[
+              { required: true, message: t('pleaseInputWhitepaper') },
+              { type: 'url', message: t('invalidUrl') }
+            ]}
+          >
+            <Input placeholder={t('whitepaperPlaceholder')} />
+          </Form.Item>
+        </Space>
       </Form>
     </Modal>
   );
 };
 
-export default UpdateCurrencyModal;
+export default UpdateCryptoCurrencyModal;
--- productx/src/views/base/sysWallets/WalletsCreateFormModel.js
+++ productx/src/views/base/sysWallets/WalletsCreateFormModel.js
@@ -1,15 +1,40 @@
-import React, { useEffect, useState } from 'react';
-import { Modal, Form, Input, Select, message } from 'antd';
-import api from 'src/axiosInstance';
+import React from 'react';
+import { Modal, Form, Input, Select, Space } from 'antd';
+import { useTranslation } from 'react-i18next';
+
+const { Option } = Select;
 
 const WalletCreateFormModal = ({
-                                 isVisible,
-                                 onCancel,
-                                 onFinish,
-                                 form,
-                                 countries,
-                                 cryptoCurrencies
-                               }) => {
+  isVisible,
+  onCancel,
+  onFinish,
+  form,
+  countries,
+  cryptoCurrencies
+}) => {
+  const { t } = useTranslation();
+
+  // 渲染国家选项
+  const countryOption = (country) => (
+    <Option key={country.code} value={country.code}>
+      <Space>
+        <img 
+          src={country.flagImageUrl} 
+          alt={country.name}
+          style={{ 
+            width: 20, 
+            height: 15, 
+            objectFit: 'cover',
+            borderRadius: 2,
+            border: '1px solid #f0f0f0'
+          }}
+        />
+        <span>{country.name}</span>
+        <span style={{ color: '#999' }}>({country.code})</span>
+      </Space>
+    </Option>
+  );
+
   return (
     <Modal
       title="新增钱包"
@@ -17,13 +42,31 @@ const WalletCreateFormModal = ({
       onCancel={onCancel}
       onOk={() => form.submit()}
     >
-      <Form form={form} onFinish={onFinish}>
+      <Form 
+        form={form} 
+        onFinish={onFinish}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="密码"
+          name="password"
+          rules={[{ required: true, message: '请输入钱包密码' }]}
+        >
+          <Input.Password 
+            placeholder="请输入密码" 
+            autoComplete="new-password"
+          />
+        </Form.Item>
+
         <Form.Item
           label="钱包类型"
           name="type"
           rules={[{ required: true, message: '请选择钱包类型' }]}
         >
-          <Select placeholder="请选择钱包类型" allowClear>
+          <Select 
+            placeholder="请选择钱包类型" 
+            allowClear
+          >
             {cryptoCurrencies.map((crypto) => (
               <Select.Option key={crypto.id} value={crypto.id}>
                 {crypto.name} ({crypto.symbol})
@@ -31,33 +74,45 @@ const WalletCreateFormModal = ({
             ))}
           </Select>
         </Form.Item>
+
         <Form.Item
           label="钱包标签"
           name="label"
           rules={[{ required: true, message: '请输入钱包标签' }]}
         >
-          <Input placeholder="例如：My Wallet" />
+          <Input 
+            placeholder="例如：My Wallet" 
+            autoComplete="off"
+          />
         </Form.Item>
+
         <Form.Item
           label="国家码"
           name="countryCode"
           rules={[{ required: true, message: '请选择国家码' }]}
         >
           <Select
-            placeholder="请选择国家" allowClear>
-            {countries.map((country) => (
-              <Select.Option key={country.code} value={country.code}>
-                {country.name} ({country.code})
-              </Select.Option>
-            ))}
+            showSearch
+            placeholder="请选择国家"
+            allowClear
+            optionFilterProp="children"
+            filterOption={(input, option) => {
+              const country = countries.find(c => c.code === option.value);
+              return (
+                country?.name.toLowerCase().includes(input.toLowerCase()) ||
+                country?.code.toLowerCase().includes(input.toLowerCase())
+              );
+            }}
+            dropdownMatchSelectWidth={false}
+            popupMatchSelectWidth={false}
+            listHeight={256}
+            dropdownStyle={{ 
+              minWidth: 250,
+              maxWidth: 300
+            }}
+          >
+            {countries.map(country => countryOption(country))}
           </Select>
-        </Form.Item>
-        <Form.Item
-          label="密码"
-          name="password"
-          rules={[{ required: true, message: '请输入钱包密码' }]}
-        >
-          <Input.Password placeholder="请输入密码" />
         </Form.Item>
       </Form>
     </Modal>
